f6548addd8e3e8069e640bb6fbc8b1fb77648a7e
Andres Betancourt <aebb001@gmail.com>
2020-12-17 12:16:01 -0400

    Carousel with glider

---
+++ js/app.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", () => {
+  new Glider(document.querySelector(".carousel__lista"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // draggable: true,
+    dots: ".carousel__indicadores",
+    arrows: {
+      prev: ".carousel__anterior",
+      next: ".carousel__siguiente",
+    },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 800,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          itemWidth: 150,
+          duration: 0.25,
+        },
+      },
+      {
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          itemWidth: 150,
+          duration: 0.25,
+          rewind: true,
+        },
+      },
+    ],
+  });
+});
